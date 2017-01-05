9e4debdab3313ad02620c8c170142caf58ec67df
eason <zszml454@gmail.com>
2017-01-05 16:25:18 +0800

    add repostitory

---
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/repository/weeklyreport/FinishedRecordRepository.java
@@ -0,0 +1,12 @@
+package com.asiainfo.repository.weeklyreport;
+
+import com.asiainfo.domain.entity.weeklyreport.FinishedRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by eason on 2017/1/5.
+ */
+@RepositoryRestResource(collectionResourceRel = "finishedRecords",path = "finishedRecordsß")
+public interface FinishedRecordRepository extends JpaRepository<FinishedRecord,Long> {
+}
