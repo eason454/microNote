dbeb4900919e3fe19fcd8247b341aaf88f921b32
zhangyi <zhangyi@asiainfo.com>
2017-01-06 16:24:24 +0800

    add to

---
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/repository/microRecord/RecordAttachmentRepository.java
@@ -0,0 +1,10 @@
+package com.asiainfo.repository.microRecord;
+
+import com.asiainfo.domain.entity.microRecord.RecordAttachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by eason on 2017/1/6.
+ */
+public interface RecordAttachmentRepository extends JpaRepository<RecordAttachment,Long> {
+}
---
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/impl/WeeklyReportServiceimpl.java
@@ -0,0 +1,23 @@
+package com.asiainfo.service.weeklyreport.impl;
+
+import com.asiainfo.domain.entity.weeklyreport.WeeklyReport;
+import com.asiainfo.repository.weeklyreport.WeeklyReportRepository;
+import com.asiainfo.service.weeklyreport.interfaces.WeeklyReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by eason on 2017/1/6.
+ */
+@Service
+public class WeeklyReportServiceimpl implements WeeklyReportService {
+	@Autowired
+	private WeeklyReportRepository weeklyReportRepository;
+
+	@Override
+	public WeeklyReport createWeeklyReport(long reportUserId) {
+		// 构造WeeklyReport对象
+		WeeklyReport weeklyReport = new WeeklyReport(reportUserId);
+		return weeklyReportRepository.save(weeklyReport);
+	}
+}
