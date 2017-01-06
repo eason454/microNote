f1d38f6e0acfb0fb16973b75d5f47d94b4d263ad
eason <zszml454@gmail.com>
2017-01-06 16:03:38 +0800

    add

--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/controller/weeklyreport/WeeklyReportController.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/controller/weeklyreport/WeeklyReportController.java
@@ -3,10 +3,7 @@ package com.asiainfo.controller.weeklyreport;
 import com.asiainfo.domain.entity.weeklyreport.WeeklyReport;
 import com.asiainfo.service.weeklyreport.interfaces.WeeklyReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by eason on 2017/1/6.
@@ -16,7 +13,7 @@ public class WeeklyReportController {
     @Autowired
     private WeeklyReportService weeklyReportService;
     @RequestMapping(path = "/createWeeklyReport",method = RequestMethod.POST)
-    public WeeklyReport createWeeklyReport(@PathVariable("userId") long reportUserId){
+    public WeeklyReport createWeeklyReport(@RequestParam("userId") long reportUserId){
      return  weeklyReportService.createWeeklyReport(reportUserId);
     }
 }
