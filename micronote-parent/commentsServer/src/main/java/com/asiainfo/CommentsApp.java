e13e756628607cff63f50ccefdde82883b45dab5
zhangyi <zhangyi@asainfo.com>
2017-01-09 21:18:35 +0800

    udpate

--- micronote-parent/commentsServer/src/main/java/com/asiainfo/CommentsApp.java
+++ micronote-parent/commentsServer/src/main/java/com/asiainfo/CommentsApp.java
@@ -3,6 +3,7 @@ package com.asiainfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * 评论应用
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaRepositories
 public class CommentsApp 
 {
     public static void main( String[] args )
