79cc3163d617acc12dfcee66f9c9243b3e4fcbb2
jerry <303301416@qq.com>
2017-01-09 17:32:32 +0800

    commit for a pull

--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/impl/ReportRecordServiceImpl.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/impl/ReportRecordServiceImpl.java
@@ -14,4 +14,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ReportRecordServiceImpl implements IReportRecordService {
+    @Autowired
+    ReportRecordRepository reportRecordRepository;
+
+    @Override
+    public ReportRecord createReportRecord(ReportRecord reportRecord) {
+        return reportRecordRepository.save(reportRecord);
+    }
+
+    @Override
+    public boolean modifyReportRecord(ReportRecord reportRecord) {
+        /*
+        1、根据ID获取到旧数据记录
+        2、过滤空值
+        3、将新值传入到旧的对象里面
+        4、调用Repository保存
+         */
+        ReportRecord oldRecord = reportRecordRepository.findOne(reportRecord.getReportRecordId());
+        String[] nullProperties = CommonUtils.getNullPropertyNames(reportRecord);
+        BeanUtils.copyProperties(reportRecord,oldRecord,nullProperties);
+        reportRecordRepository.save(oldRecord);
+        return false;
+    }
+
+    @Override
+    public boolean deleteReportRecord(long reportRecordId) {
+        reportRecordRepository.delete(reportRecordId);
+        return true;
+    }
 }
--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/interfaces/IReportRecordService.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/interfaces/IReportRecordService.java
@@ -1,10 +1,23 @@
 package com.asiainfo.service.weeklyreport.interfaces;
 
 import com.asiainfo.domain.entity.weeklyreport.ReportRecord;
+import org.springframework.boot.actuate.endpoint.AutoConfigurationReportEndpoint;
 
 /**
  * Created by eason on 2017/1/9.
  */
 public interface IReportRecordService {
+    /*
+    修改周报项（成果类）
+     */
+    public boolean modifyReportRecord(ReportRecord reportRecord);
+    /*
+    删除周报项（成果类）
+     */
+    public boolean deleteReportRecord(long reportRecordId);
+    /*
+    增加周报项（成果类）
+     */
+    public ReportRecord createReportRecord(ReportRecord reportRecord);
 
 }
