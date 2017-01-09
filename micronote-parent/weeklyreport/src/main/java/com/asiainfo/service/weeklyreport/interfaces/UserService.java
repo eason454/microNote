d8a258e76c8aa464389a78cb856ea487c6f62e55
zhangyi <zhangyi@asainfo.com>
2017-01-09 16:31:59 +0800

    update

--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/interfaces/UserService.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/service/weeklyreport/interfaces/UserService.java
@@ -1,10 +1,8 @@
 package com.asiainfo.service.weeklyreport.interfaces;
 
-import com.asiainfo.domain.entity.user.User;
-import com.asiainfo.repository.weeklyreport.WeeklyReportRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
+
+import com.asiainfo.domain.entity.user.User;
 
 /**
  * Created by eason on 2017/1/6.
