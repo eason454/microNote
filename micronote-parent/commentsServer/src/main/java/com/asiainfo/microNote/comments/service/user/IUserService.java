6968063f0400ceaca0fea105a5502dc89568b40e
zhangyi <zhangyi@asainfo.com>
2017-01-11 14:06:01 +0800

    update

--- micronote-parent/commentsServer/src/main/java/com/asiainfo/microNote/comments/service/user/IUserService.java
+++ micronote-parent/commentsServer/src/main/java/com/asiainfo/microNote/comments/service/user/IUserService.java
@@ -1,5 +1,8 @@
 package com.asiainfo.microNote.comments.service.user;
 
-public class IUserService {
+import org.springframework.cloud.netflix.feign.FeignClient;
 
+@FeignClient("weeklyreport")
+public interface IUserService {
+	
 }
