10341aed4d9121d7a3e5eccd818bc0f8d6342f00
eason <zszml454@gmail.com>
2017-01-11 15:27:16 +0800

    add kara response message

---
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/util/kara/MessageConstructor.java
@@ -0,0 +1,37 @@
+package com.asiainfo.util.kara;
+
+import com.asiainfo.domain.response.KaraAttachment;
+import com.asiainfo.domain.response.KaraField;
+import com.asiainfo.domain.response.KaraMessage;
+import com.asiainfo.util.consts.CommonConst;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eason on 2017/1/11.
+ */
+public class MessageConstructor {
+    public static KaraMessage constructMessageWithFields(List<KaraField> karaFields){
+        KaraMessage message=new KaraMessage();
+//        message.setChannel(body.get("channel_id"));
+        message.setText(CommonConst.KaraInfo.querySuccess);
+        KaraAttachment attach=new KaraAttachment();
+        attach.setTitle(CommonConst.KaraInfo.nextWeeklyInfo);
+//        attach.setCallbackId("testcallbackId");  //回调id填什么呢
+        List<KaraField> fieldList=new ArrayList<KaraField>();
+        for (KaraField karaField :
+                karaFields) {
+            KaraField field=new KaraField();
+            field.setTitle(karaField.getTitle());
+            field.setValue(karaField.getValue());
+            fieldList.add(field);
+        }
+        attach.setFields(fieldList.toArray(new KaraField[fieldList.size()]));
+        //不支持attachments时显示的内容
+        List<KaraAttachment> attachList=new ArrayList<KaraAttachment>();
+        attachList.add(attach);
+        message.setAttachments(attachList.toArray(new KaraAttachment[0]));
+        return message;
+    }
+}
