2db2f96156ce4b9fbb37bb44f02ecc0d00553577
eason <zszml454@gmail.com>
2017-01-12 17:06:16 +0800

    add properties,delete yml

--- micronote-parent/gateway/src/main/java/com/asiainfo/GatewayApplication.java
+++ micronote-parent/gateway/src/main/java/com/asiainfo/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.asiainfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.context.annotation.Bean;
 
