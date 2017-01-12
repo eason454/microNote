885d97f64fb8c980ab2fd04235deabf37c16e78c
eason <zszml454@gmail.com>
2017-01-12 09:43:18 +0800

    add kara response message

--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/WeeklyreportApplication.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/WeeklyreportApplication.java
@@ -2,8 +2,11 @@ package com.asiainfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +14,9 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 public class WeeklyreportApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeeklyreportApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return  builder.build();
 	}
 }
--- micronote-parent/weeklyreport/src/main/java/com/asiainfo/controller/weeklyreport/WeeklyReportToController.java
+++ micronote-parent/weeklyreport/src/main/java/com/asiainfo/controller/weeklyreport/WeeklyReportToController.java
@@ -2,9 +2,17 @@ package com.asiainfo.controller.weeklyreport;
 
 import com.asiainfo.domain.entity.user.User;
 import com.asiainfo.domain.kara.KaraRequestObject;
+import com.asiainfo.domain.kara.response.KaraField;
+import com.asiainfo.domain.kara.response.KaraMessage;
+import com.asiainfo.domain.kara.response.KaraUserInfo;
+import com.asiainfo.domain.kara.response.KaraUserResponseInfo;
+import com.asiainfo.restclient.RestKaraUrl;
 import com.asiainfo.service.weeklyreport.interfaces.IWeeklyReportToService;
 import com.asiainfo.service.weeklyreport.interfaces.UserService;
+import com.asiainfo.util.consts.CommonConst;
+import com.asiainfo.util.kara.MessageConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.asiainfo.domain.entity.weeklyreport.WeeklyReportTo;
 import com.asiainfo.repository.weeklyreport.WeeklyReportToRepository;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class WeeklyReportToController {
@@ -20,21 +32,37 @@ public class WeeklyReportToController {
 	private IWeeklyReportToService weeklyReportToService;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private RestTemplate restTemplate;
 	@RequestMapping(path = "/setWeeklyReportTo", method = RequestMethod.POST)
-	public String setWeeklyReportTo(@RequestBody KaraRequestObject request){
+	public KaraMessage setWeeklyReportTo(@RequestBody KaraRequestObject request){
+        String resultMessage="";
 		String userId=request.getUserId();
-		String auditUserId=request.getText();
+		String auditStaffNumber=request.getText();
+        String auditUserId="";
+        List<KaraField> karaFieldList=new ArrayList<KaraField>();
+        KaraUserResponseInfo userResponseInfo=restTemplate.getForObject(RestKaraUrl.StaffInfoByStaffId,KaraUserResponseInfo.class,auditStaffNumber);
+        if(!userResponseInfo.getResponseCode().equals(CommonConst.KaraInfo.responseSuccessCode) || StringUtils.isEmpty(userResponseInfo.getStaffResponseInfo().getAccountId())){
+            resultMessage=CommonConst.KaraInfo.userNotExists;
+            //// TODO: 2017/1/11 需要拼装karaMessage
+            KaraField field=new KaraField();
+            field.setTitle(CommonConst.KaraInfo.setWeeklyReportResult);
+            field.setValue(String.format(CommonConst.KaraInfo.SetWeeklyReportResultFail,resultMessage));
+            karaFieldList.add(field);
+            return MessageConstructor.constructMessageWithFields("",karaFieldList);
+        }else{
+            auditUserId=userResponseInfo.getStaffResponseInfo().getAccountId();
+        }
 		WeeklyReportTo weeklyReportTo = weeklyReportToService.findByReportUserId(userId);
 		weeklyReportTo.setAuditingUserId(auditUserId);
 		//查询汇报对象是否已经在微记用户中存在
 		User user=userService.queryUserById(auditUserId);
-		if(user==null){
-			//调用kara查询用户信息
-		}
+
 		boolean flag=weeklyReportToService.saveWeeklyReportTo(weeklyReportTo);
 		if(flag){
 
 		}
-		return "OK";
+		//// TODO: 2017/1/11 增加拼装KaraMessage
+        return null;
 	}
 }
